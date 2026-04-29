b7b5d3778817ba807c350e81e7683e1ec8c58af4
Sakexxpearex <bastij58@gmail.com>
2026-04-29 02:22:02 +0000

    Se edito app.blade para la renderizacion de react, cambio en la ubicacion de la tabla sessions, de user a laravel para que no de error

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    protected $connection = "usuarios";
+    
 
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create("users", function (Blueprint $table) {
+        Schema::connection('usuarios')->create("users", function (Blueprint $table) {
             $table->id();
             $table->string("nombre_1");
             $table->string("nombre_2");
@@ -29,13 +29,13 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create("password_reset_tokens", function (Blueprint $table) {
+        Schema::connection('usuarios')->create("password_reset_tokens", function (Blueprint $table) {
             $table->string("email")->primary();
             $table->string("token");
             $table->timestamp("created_at")->nullable();
         });
 
-        Schema::create("sessions", function (Blueprint $table) {
+        Schema::connection('laravel')->create("sessions", function (Blueprint $table) {
             $table->string("id")->primary();
             $table->foreignId("user_id")->nullable()->index();
             $table->string("ip_address", 45)->nullable();
@@ -50,8 +50,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists("users");
-        Schema::dropIfExists("password_reset_tokens");
-        Schema::dropIfExists("sessions");
+        Schema::connection('usuarios')->dropIfExists("users");
+        Schema::connection('usuarios')->dropIfExists("password_reset_tokens");
+        Schema::connection('laravel')->dropIfExists("sessions");
     }
 };
